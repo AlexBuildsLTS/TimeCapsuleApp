6555d8cea038eb8764ae31cef3c0be8c5800ba31
AlexBuildsLTS <alex.youssef@live.com>
2025-08-02 03:27:13 +0200

    Install the missing dependency: I will add babel-plugin-module-resolver to your project's devDependencies. will create a babel.config.js file to configure Babel to use this new plugin, ensuring that your path aliases are correctly resolved. updated tsconfig paths

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@/app': './app',
+            '@/assets': './assets',
+            '@/components': './components',
+            '@/config': './config',
+            '@/constants': './constants',
+            '@/hooks': './hooks',
+            '@/services': './services',
+            '@/store': './store',
+            '@/types': './types',
+          },
+        },
+      ],
+    ],
+  };
+};
